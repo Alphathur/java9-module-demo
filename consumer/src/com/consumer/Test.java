7d935e08154df4dc826bccbe0ae597d775faefa8
Alphathur <1274221961@qq.com>
2020-04-05 23:52:54 +0800

    add comments

--- consumer/src/com/consumer/Test.java
+++ consumer/src/com/consumer/Test.java
@@ -12,6 +12,8 @@ public class Test {
         Calculator minus  = new Minus ();
         System.out.println (minus.getName ());
 
+        // you can not use Div unless you exports package 'com.provider.other' in module-info.java under provider module
+        // a parent package export takes no effects on subpackage
 //        Calculator div = new Div ();
 //        System.out.println (div.getName ());
     }
